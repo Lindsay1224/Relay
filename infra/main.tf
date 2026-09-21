65dd4d5c920e14194cc90fc32cd23e48599ede2f
Lindsay1224 <lscarangello@gmail.com>
2026-09-20 22:18:15 -0400

    Grant worker task fan-out permissions

--- infra/main.tf
+++ infra/main.tf
@@ -5,6 +5,7 @@ variable "project_id" { type = string }
 variable "region" { type = string default = "us-east4" }
 variable "worker_image" { type = string }
 provider "google" { project = var.project_id, region = var.region }
+data "google_project" "current" { project_id = var.project_id }
 
 locals { services = ["cloudtasks.googleapis.com", "run.googleapis.com", "cloudkms.googleapis.com", "secretmanager.googleapis.com", "gmail.googleapis.com", "aiplatform.googleapis.com", "firestore.googleapis.com"] }
 resource "google_project_service" "required" { for_each = toset(local.services); service = each.value; disable_on_destroy = false }
@@ -18,9 +19,12 @@ resource "google_kms_crypto_key_iam_member" "app_encrypt" { crypto_key_id = goog
 resource "google_project_iam_member" "worker_firestore" { project = var.project_id role = "roles/datastore.user" member = "serviceAccount:${google_service_account.worker.email}" }
 resource "google_project_iam_member" "app_firestore" { project = var.project_id role = "roles/datastore.user" member = "serviceAccount:firebase-app-hosting-compute@${var.project_id}.iam.gserviceaccount.com" }
 resource "google_project_iam_member" "app_tasks" { project = var.project_id role = "roles/cloudtasks.enqueuer" member = "serviceAccount:firebase-app-hosting-compute@${var.project_id}.iam.gserviceaccount.com" }
+resource "google_project_iam_member" "worker_tasks" { project = var.project_id role = "roles/cloudtasks.enqueuer" member = "serviceAccount:${google_service_account.worker.email}" }
 resource "google_project_iam_member" "worker_vertex" { project = var.project_id role = "roles/aiplatform.user" member = "serviceAccount:${google_service_account.worker.email}" }
-resource "google_cloud_run_v2_service" "worker" { name = "relay-extraction-worker" location = var.region ingress = "INGRESS_TRAFFIC_INTERNAL_ONLY" template { service_account = google_service_account.worker.email containers { image = var.worker_image env { name = "RELAY_TASKS_QUEUE" value = google_cloud_tasks_queue.gmail.name } } } depends_on = [google_project_service.required] }
+resource "google_cloud_run_v2_service" "worker" { name = "relay-extraction-worker" location = var.region ingress = "INGRESS_TRAFFIC_INTERNAL_ONLY" template { service_account = google_service_account.worker.email containers { image = var.worker_image env { name = "RELAY_TASKS_QUEUE" value = google_cloud_tasks_queue.gmail.name } env { name = "RELAY_WORKER_URL" value = "https://relay-extraction-worker-${data.google_project.current.number}.${var.region}.run.app" } } } depends_on = [google_project_service.required] }
 resource "google_cloud_run_v2_service_iam_member" "tasks_invoker" { name = google_cloud_run_v2_service.worker.name location = var.region role = "roles/run.invoker" member = "serviceAccount:${google_service_account.tasks.email}" }
 resource "google_service_account_iam_member" "app_can_mint_tasks_oidc" { service_account_id = google_service_account.tasks.name role = "roles/iam.serviceAccountTokenCreator" member = "serviceAccount:firebase-app-hosting-compute@${var.project_id}.iam.gserviceaccount.com" }
 resource "google_service_account_iam_member" "app_can_act_as_tasks_oidc" { service_account_id = google_service_account.tasks.name role = "roles/iam.serviceAccountUser" member = "serviceAccount:firebase-app-hosting-compute@${var.project_id}.iam.gserviceaccount.com" }
+resource "google_service_account_iam_member" "worker_can_mint_tasks_oidc" { service_account_id = google_service_account.tasks.name role = "roles/iam.serviceAccountTokenCreator" member = "serviceAccount:${google_service_account.worker.email}" }
+resource "google_service_account_iam_member" "worker_can_act_as_tasks_oidc" { service_account_id = google_service_account.tasks.name role = "roles/iam.serviceAccountUser" member = "serviceAccount:${google_service_account.worker.email}" }
 output "worker_url" { value = google_cloud_run_v2_service.worker.uri }
